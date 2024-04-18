d68c9d19f9147fc10c6a3df56c36deacb3b85191
danilo012 <46241643+danilo012@users.noreply.github.com>
2024-04-18 11:02:38 +0800

    Update shuffle-worker.ts

--- src/workers/shuffle-worker.ts
+++ src/workers/shuffle-worker.ts
@@ -5,6 +5,7 @@ export type WorkerPostMessageData = {
   data: Symbol[][];
 };
 
+
 /* eslint-disable-next-line no-restricted-globals */
 self.onmessage = (event: MessageEvent) => {
   const reels = getShuffledReels();
@@ -12,3 +13,24 @@ self.onmessage = (event: MessageEvent) => {
   /* eslint-disable-next-line no-restricted-globals */
   self.postMessage({ data: reels });
 };
+
+/*   const worker = useMemo(
+    () =>
+      new Worker(new URL('../../../workers/audio-worker.ts', import.meta.url), {
+        type: 'module',
+      }),
+    []
+  );
+
+  const handleOnClick = useCallback(
+    (event: MouseEvent<HTMLButtonElement>) => {
+      if (buttonSound) {
+        worker.postMessage(JSON.parse(JSON.stringify(buttonSound)));
+        console.log(777);
+        // buttonSound.play();
+      }
+      onClick(event);
+    },
+    [buttonSound, worker, onClick]
+  );
+ */
